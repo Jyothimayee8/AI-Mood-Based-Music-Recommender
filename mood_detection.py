bcc7ec27c39e3f5fb8f405e1d4275f3101fea432
Jyothimayee8 <146567709+Jyothimayee8@users.noreply.github.com>
2025-05-24 05:23:00 +0530

    mood_detection.py

--- mood_detection.py
+++ mood_detection.py
@@ -17,19 +17,3 @@ def detect_mood_face(frame) -> str:
         print("DeepFace error:", e)
     return None
 
-'''
-def detect_mood_face(frame) -> str:
-    try:
-        rgb_frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
-        resized_frame = cv2.resize(rgb_frame, (640, 480))
-        analysis = DeepFace.analyze(resized_frame, actions=["emotion"], enforce_detection=False)
-
-        if isinstance(analysis, list):
-            analysis = analysis[0]
-
-        if "dominant_emotion" in analysis:
-            return analysis["dominant_emotion"]
-    except Exception as e:
-        print("DeepFace error:", e)
-    return None
-'''
